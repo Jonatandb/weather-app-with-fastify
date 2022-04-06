112109d264014a3f8ee0eb9a986724ec8c0f2024
Jonatandb <jonatandb@gmail.com>
2022-04-06 02:45:33 -0300

    refactor: :lipstick: Refactor and styling of WeatherData component

--- frontend/src/components/WeatherData/WeatherData.js
+++ frontend/src/components/WeatherData/WeatherData.js
@@ -1,32 +1,50 @@
 import React from 'react'
 import './WeatherData.css'
 export default function WeatherData({ isLoading, weatherData }) {
-  if (isLoading && weatherData && !Object.keys(weatherData).length)
-    return <h2>Cargando datos del clima... ⏳</h2>
-  if (!weatherData || !Object.keys(weatherData).length)
-    return <h2>Error al obtener los datos del clima 💀</h2>
+  if (isLoading) return <div>⏳ Cargando datos del clima... </div>
+  if (!weatherData || !Object.keys(weatherData).length) return null
 
   const { main, wind, weather } = weatherData
   const { temp, humidity, pressure } = main
   const { speed } = wind
   const { main: weatherMain, description } = weather[0]
 
-  const dt1000 = weatherData.dt * 1000
-  const timezone1000 = weatherData.timezone * 1000
-  const date = new Date(dt1000 - timezone1000).toLocaleDateString()
-  const time = new Date(dt1000 - timezone1000 - 10000000).toLocaleTimeString()
+  const iconURL = `https://openweathermap.org/img/wn/${weather[0].icon.replace('n', 'd')}@2x.png`
 
   return (
-    <div className='weatherData-Container'>
-      <h4>{isLoading ? 'Cargando datos del clima... ⏳' : 'Datos del clima 🌡️'}</h4>
-      <p className='dataRow'>{isLoading ? '' : `Temperatura actual: ${temp}ºC`}</p>
-      <p className='dataRow'>{isLoading ? '' : `Humedad: ${humidity}%`}</p>
-      <p className='dataRow'>{isLoading ? '' : `Presión: ${pressure}hPa`}</p>
-      <p className='dataRow'>{isLoading ? '' : `Viento: ${speed}m/s`}</p>
-      <p className='dataRow'>{isLoading ? '' : `Estado del cielo: ${weatherMain}`}</p>
-      <p className='dataRow'>{isLoading ? '' : `Descripción del cielo: ${description}`}</p>
-      <p className='dataRow'>{isLoading ? '' : `Fecha: ${date}`}</p>
-      <p className='dataRow'>{isLoading ? '' : `Hora: ${time}`}</p>
+    <div className='WeatherData-Container'>
+      <h4 className='WeatherData-Title'>Datos del clima 🌡️</h4>
+      <div className='WeatherData-Details-Container'>
+        <div className='WeatherData-Image-Container'>
+          <img src={iconURL} alt={`${weather[0].description} ${weatherData.main.temp} `}></img>
+        </div>
+        <div className='WeatherData-Rows-Container'>
+          <div className='WeatherData-Row'>
+            <p>Temperatura:</p>
+            <p>{`${temp}ºC`}</p>
+          </div>
+          <div className='WeatherData-Row'>
+            <p>Humedad: </p>
+            <p>{`${humidity}%`}</p>
+          </div>
+          <div className='WeatherData-Row'>
+            <p>Presión: </p>
+            <p>{`${pressure}hPa`}</p>
+          </div>
+          <div className='WeatherData-Row'>
+            <p>Viento: </p>
+            <p>{`${speed}m/s`}</p>
+          </div>
+          <div className='WeatherData-Row'>
+            <p>Estado: </p>
+            <p>{`${weatherMain}`}</p>
+          </div>
+          <div className='WeatherData-Row'>
+            <p>Cielo: </p>
+            <p>{`${description}`}</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- frontend/src/components/WeatherData/WeatherData.test.js
+++ frontend/src/components/WeatherData/WeatherData.test.js
@@ -3,39 +3,39 @@ const { default: WeatherData } = require('./WeatherData')
 
 describe('WeatherData', () => {
   it('should render correctly', () => {
-    const { asFragment } = render(
-      <WeatherData
-        isLoading={false}
-        weatherData={{
-          main: {
-            temp: 10,
-            humidity: 20,
-            pressure: 30,
-          },
-          wind: {
-            speed: 40,
-          },
-          weather: [
-            {
-              main: 'Clouds',
-              description: 'scattered clouds',
-            },
-          ],
-          dt: 1598784800,
-          timezone: 3600,
-        }}
-      />
-    )
-    expect(asFragment()).toMatchSnapshot()
+    const { container } = render(<WeatherData />)
+    expect(container).toMatchSnapshot()
   })
 
-  it('should render correctly when isLoading is true', () => {
-    const { asFragment } = render(<WeatherData isLoading weatherData={{}} />)
-    expect(asFragment()).toMatchSnapshot()
+  it('should render correctly with data', () => {
+    const weatherData = {
+      main: {
+        temp: 10,
+        humidity: 10,
+        pressure: 10,
+      },
+      wind: {
+        speed: 10,
+      },
+      weather: [
+        {
+          main: 'main',
+          description: 'description',
+          icon: 'icon',
+        },
+      ],
+    }
+    const { container } = render(<WeatherData weatherData={weatherData} />)
+    expect(container).toMatchSnapshot()
   })
 
-  it('should render correctly when weatherData is empty', () => {
-    const { asFragment } = render(<WeatherData weatherData={{}} />)
-    expect(asFragment()).toMatchSnapshot()
+  it('should render correctly with loading', () => {
+    const { container } = render(<WeatherData isLoading />)
+    expect(container).toMatchSnapshot()
+  })
+
+  it('should render correctly with empty data', () => {
+    const { container } = render(<WeatherData weatherData={{}} />)
+    expect(container).toMatchSnapshot()
   })
 })
