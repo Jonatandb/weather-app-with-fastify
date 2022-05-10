c9b42d1f373d65bda2f932d707636d7990c4ac98
Jonatandb <jonatandb@gmail.com>
2022-05-10 00:12:01 -0300

    fix: :bug: Added missing space

--- frontend/src/components/WeatherData/WeatherData.js
+++ frontend/src/components/WeatherData/WeatherData.js
@@ -8,27 +8,33 @@ export default function WeatherData({ isLoading, weatherData }) {
     <div className='WeatherData-Container'>
       {isLoading ? (
         <div className='WeatherData-LoadingMessage'>
-          <SpinningIcon speed='fast'>⏳</SpinningIcon> <p>Cargando datos del clima...</p>
+          <SpinningIcon speed='fast'>⏳</SpinningIcon>{' '}
+          <p>Cargando datos del clima...</p>
         </div>
       ) : processedWeatherData ? (
         <>
           <h4 className='WeatherData-Title'>Datos del clima 🌡️</h4>
           <div className='WeatherData-Details-Container'>
             <div className='WeatherData-Image-Container'>
-              <img src={processedWeatherData.icon.url} alt={processedWeatherData.icon.alt} />
+              <img
+                src={processedWeatherData.icon.url}
+                alt={processedWeatherData.icon.alt}
+              />
             </div>
             <div className='WeatherData-Rows-Container'>
-              {processedWeatherData.items.map(({ description, value, suffix }) => (
-                <div className='WeatherData-Row' key={description}>
-                  <p>{description}:</p>
-                  <p>{`${value}${suffix}`}</p>
-                </div>
-              ))}
+              {processedWeatherData.items.map(
+                ({ description, value, suffix }) => (
+                  <div className='WeatherData-Row' key={description}>
+                    <p>{description}:</p>
+                    <p>{`${value} ${suffix}`}</p>
+                  </div>
+                ),
+              )}
             </div>
           </div>
         </>
       ) : (
-        <div>{/*/Datos inválidos...*/}</div>
+        <div>{/* Datos inválidos... */}</div>
       )}
     </div>
   )
